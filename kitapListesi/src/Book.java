47b19bbe465c411191ffdeb21ab16bd9eb2586f2
nurullhkrds <115217234+nurullhkrds@users.noreply.github.com>
2023-06-04 18:29:59 +0300

    new

---
+++ kitapListesi/src/Book.java
@@ -0,0 +1,45 @@
+public class Book {
+    private String name;
+    private String author;
+    private int numberPage;
+    private String releaseDate;
+
+    public Book(String name, int numberPage, String author, String releaseDate) {
+        this.name = name;
+        this.author = author;
+        this.numberPage = numberPage;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberPage() {
+        return numberPage;
+    }
+
+    public void setNumberPage(int numberPage) {
+        this.numberPage = numberPage;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
